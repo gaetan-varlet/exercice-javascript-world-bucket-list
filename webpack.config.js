79275c63b4e795fdff1371a6ec991e8aaecd082d
Gaëtan Varlet <gaetanvarlet@gmail.com>
2018-11-08 20:44:06 +0100

    ajout du déploiement avec gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ module.exports = {
     watch: true,
     entry: "./src/index.js", // dit à webpack le point d'entrée de notre projet
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build"),
         filename: "bundle.js" // dit à webpack le nom du fichier produit
     },
     devtool: "source-map",
     devServer: {
-        contentBase: path.resolve(__dirname, "dist"), // dit au serveur de nous servir ce qu'il y a dans le dossier dist au lieu de la racine du projet
+        contentBase: path.resolve(__dirname, "build"), // dit au serveur de nous servir ce qu'il y a dans le dossier dist au lieu de la racine du projet
         open: true // lance le navigateur automatiquement au lancement du serveur, ce qui évite de mettre --open dans la tâche
     }
 }
